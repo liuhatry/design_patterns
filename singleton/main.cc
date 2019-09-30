cef163203496080288f9a68ede1e5831c91aa915
hatry <liuhatry@163.com>
2019-09-30 20:14:05 +0800

    test

--- singleton/main.cc
+++ singleton/main.cc
@@ -2,10 +2,10 @@
 
 
 int main(){
-	singleton * s1 = singleton::getInstance();
-	singleton * s2 = singleton::getInstance();
-	if (s1 == s2){
-		cout<<"equal!"<<endl;
-	}
-	return 0;
+  singleton * s1 = singleton::getInstance();
+  singleton * s2 = singleton::getInstance();
+  if (s1 == s2) {
+    cout<<"equal!"<<endl;
+  }
+  return 0;
 }
